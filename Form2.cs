68da5d992ca07237b8062966acaeab2d268572b2
Harika Dubba <51993052+Harika-murthy@users.noreply.github.com>
2020-08-04 23:53:58 +0530

    Add files via upload

---
+++ Form2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace loginform
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'bankingDataSet.Table_1' table. You can move, or remove it, as needed.
+            this.table_1TableAdapter.Fill(this.bankingDataSet.Table_1);
+
+        }
+
+        
+    }
+}
